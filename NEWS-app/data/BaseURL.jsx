35d70b80b758007a3c61bfae038f882e216d8d67
Ajibola Bakare (aka Bolaji) <112806468+freakishcode@users.noreply.github.com>
2024-09-26 04:07:49 +0100

    Delete NEWS-app directory

--- NEWS-app/data/BaseURL.jsx
+++
@@ -1,5 +0,0 @@
-// TODO: DATA BASE JSON
-export const BASE_URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=2fe95fcde45746dcb281c234d6068bd0`;
-
-// ?? NOTE:When running the server USE THIS:
-//  json-server -p 5775 -w ./data/UsersData.json
--- NEWS-app/src/Pages/About.jsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return <div style={{ textAlign: "center" }}>About</div>;
-}
-
-export default About;
